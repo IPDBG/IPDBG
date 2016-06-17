0c15e5c9a181e1eedaad1446205ee01182abc26d
EvaIPDBG <eva.kissling@comlab.ch>
2016-06-17 09:27:15 +0200

    JTAG-HUB mit TAP

--- rtl/JtagHub/JTAG-HUB.vhd
+++ rtl/JtagHub/JTAG-HUB.vhd
@@ -4,12 +4,21 @@ use ieee.numeric_std.all;
 
 entity JTAG_HUB is
     generic(
-        MFF_LENGTH : natural
+        MFF_LENGTH        : natural;
+        TARGET_TECHNOLOGY : natural range 0 to 1 := '0' -- '0': "ipdbgtap"    1: spartan3
     );
     port(
        clk                : in  std_logic;
        rst                : in  std_logic;
        ce                 : in  std_logic;
+
+       TMS                : in  std_logic;
+       TCK                : in  std_logic;
+       TDI                : in  std_logic;
+       TDO                : out std_logic;
+
+
+
        DATAOUT            : out std_logic_vector(7 downto 0);
 
        Enable_LA          : out std_logic;
@@ -67,21 +76,8 @@ architecture structure of JTAG_HUB is
     end component JTAG_CDC_Komponente;
 
 
-    component BSCAN is
-        port(
-            capture : out std_logic;
-            DRCLK1  : out std_logic;
-            DRCLK2  : out std_logic;
-            RESET   : out std_logic;
-            USER1   : out std_logic;
-            USER2   : out std_logic;
-            SHIFT   : out std_logic;
-            TDI     : out std_logic;
-            Update  : out std_logic;
-            TDO1    : in  std_logic;
-            TDO2    : in  std_logic
-        );
-    end component BSCAN;
+
+
 
 
 
@@ -93,28 +89,80 @@ architecture structure of JTAG_HUB is
     signal UPDATE       : std_logic;
     signal CAPTURE      : std_logic;
     signal SHIFT        : std_logic;
-    signal TDI          : std_logic;
+    signal TDI_i        : std_logic;
     signal TDO1         : std_logic;
-    signal TDO2         : std_logic;
 
 begin
 
+    xilinx_spartan3: if TARGET_TECHNOLOGY = 1 generate -- spartan 3
+        component BSCAN is
+            port(
+                capture : out std_logic;
+                DRCLK1  : out std_logic;
+                DRCLK2  : out std_logic;
+                RESET   : out std_logic;
+                USER1   : out std_logic;
+                USER2   : out std_logic;
+                SHIFT   : out std_logic;
+                TDI     : out std_logic;
+                Update  : out std_logic;
+                TDO1    : in  std_logic;
+                TDO2    : in  std_logic
+            );
+        end component BSCAN;
+    begin
+        bscan_inst : component BSCAN
+            port map(
+                capture => CAPTURE,
+                DRCLK1  => DRCLK1,
+                DRCLK2  => DRCLK2,
+                RESET   => RESET,
+                USER1   => USER1,
+                USER2   => USER2,
+                SHIFT   => SHIFT,
+                TDI     => TDI_i,
+                Update  => UPDATE,
+                TDO1    => TDO1,
+                TDO2    => '0'
+            );
+        --TDO <= '0';
+    end generate;
 
-    bscan_inst : component BSCAN
-        port map(
-            capture => CAPTURE,
-            DRCLK1  => DRCLK1,
-            DRCLK2  => DRCLK2,
-            RESET   => RESET,
-            USER1   => USER1,
-            USER2   => USER2,
-            SHIFT   => SHIFT,
-            TDI     => TDI,
-            Update  => UPDATE,
-            TDO1    => TDO1,
-            TDO2    => TDO2
+    ipdbg_tap: if TARGET_TECHNOLOGY = 0 generate -- ipdbg-tap
+        component TAP is
+            port(
+                rst     : in  std_logic;
+                Capture : out std_logic;
+                Shift   : out std_logic;
+                Update  : out std_logic;
+                TDI_o   : out std_logic;
+                TDO_i   : in  std_logic;
+                SEL     : out std_logic;
+                DRCK    : out std_logic;
+                TDI     : in  std_logic;
+                TDO     : out std_logic;
+                TMS     : in  std_logic;
+                TCK     : in  std_logic
+            );
+        end component TAP;
+    begin
+        TAP_l : component TAP
+            port map(
+                rst     => rst,
+                Capture => CAPTURE,
+                Shift   => SHIFT,
+                Update  => UPDATE,
+                TDI_o   => TDI_i,
+                TDO_i   => TDO1,
+                SEL     => USER1,
+                DRCK    => DRCLK1,
 
-        );
+                TDI     => TDI,
+                TDO     => TDO,
+                TMS     => TMS,
+                TCK     => TCK
+            );
+    end generate;
 
 
     CDC : component JTAG_CDC_Komponente
@@ -123,7 +171,7 @@ begin
         )
         port map(
             clk                => clk,
-            rst                => rst,
+            rst                => '0',
             ce                 => ce,
 
             DATAOUT            => DATAOUT,
@@ -148,9 +196,9 @@ begin
             UPDATE             => UPDATE,
             CAPTURE            => CAPTURE,
             SHIFT              => SHIFT,
-            TDI                => TDI,
+            TDI                => TDI_i,
             TDO1               => TDO1,
-            TDO2               => TDO2
+            TDO2               => open
 
         );
 
