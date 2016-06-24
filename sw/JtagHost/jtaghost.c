467853dc2dd9f03c8fbd716f5623baafb6f91960
EvaIPDBG <eva.kissling@comlab.ch>
2016-06-24 14:57:21 +0200

    JTAG-Host for ECP2 (tested with LFE2-12E) working

--- sw/JtagHost/jtaghost.c
+++ sw/JtagHost/jtaghost.c
@@ -44,6 +44,9 @@
 
 int initSpartan3(urj_chain_t *chain);
 int initiCE40(urj_chain_t *chain);
+int initEcp2(urj_chain_t *chain);
+int initArtix7 (urj_chain_t *chain);
+int initSpartan6(urj_chain_t *chain);
 
 urj_chain_t *ipdbgJtagAllocChain(void)
 {
@@ -92,10 +95,173 @@ int ipdbgJtagInit(urj_chain_t *chain)
         return -4;
     }
 
+    if ( strcmp(part->manufacturer, "xilinx") == 0)
+    {
+        if(strcmp(part->part, "xc3s50") == 0 ||
+           strcmp(part->part, "xc3s200") == 0 ||
+           strcmp(part->part, "xc3s400") == 0 ||
+           strcmp(part->part, "xc3s1000") == 0 ||
+           strcmp(part->part, "xc3s1500") == 0 ||
+           strcmp(part->part, "xc3s2000") == 0 ||
+           strcmp(part->part, "xc3s4000") == 0 ||
+           strcmp(part->part, "xc3s5000") == 0 ||
+           strcmp(part->part, "xc3s1600e") == 0 ||
+           strcmp(part->part, "xc3s50a") == 0 ||
+           strcmp(part->part, "xc3s200a") == 0 ||
+           strcmp(part->part, "xc3s400a") == 0 ||
+           strcmp(part->part, "xc3s700a") == 0 ||
+           strcmp(part->part, "xc3s1400a") == 0 ||
+           strcmp(part->part, "xc3s100e_die") == 0 ||
+           strcmp(part->part, "xc3s500e_fg320") == 0 ||
+           strcmp(part->part, "xc3s1200e_fg320") == 0)
+            return initSpartan3(chain);
+        else if(strcmp(part->part, "xc6slx4") == 0 ||
+                strcmp(part->part, "xc6slx9") == 0 ||
+                strcmp(part->part, "xc6slx16") == 0 ||
+                strcmp(part->part, "xc6slx25") == 0 ||
+                strcmp(part->part, "xc6slx25t") == 0 ||
+                strcmp(part->part, "xc6slx45") == 0 ||
+                strcmp(part->part, "xc6slx45t") == 0 ||
+                strcmp(part->part, "xc6slx75") == 0 ||
+                strcmp(part->part, "xc6slx75t") == 0 ||
+                strcmp(part->part, "xc6slx100") == 0 ||
+                strcmp(part->part, "xc6slx100t") == 0 ||
+                strcmp(part->part, "xc6slx150") == 0 ||
+                strcmp(part->part, "xc6slx150t") == 0)
+            return initSpartan6(chain);
+//
+        else if(strcmp(part->part, "xc7a35t") == 0 ||
+                strcmp(part->part, "xc7a50t") == 0 ||
+                strcmp(part->part, "xc7a75t") == 0 ||
+                strcmp(part->part, "xc7a100t") == 0 ||
+                strcmp(part->part, "xc7a200t") == 0 )
+            return initArtix7(chain);
 
-//    return initSpartan3(chain);
+        else
+        {
+            printf("xilinx family not supported yet");
+            return -1;
+        }
+    }
+    else if ( strcmp(part->manufacturer, "Lattice Semiconductors") == 0)
+    {
+        if(strcmp(part->part, "LFE2-12E") == 0)
+            return initEcp2(chain);
+    }
+    else if ( strcmp(part->manufacturer, "ipdbg.org") == 0)
+    {
         return initiCE40(chain);
+    }
+
+    //return initEcp2(chain);
+       // return initiCE40(chain);
 }
+
+int initArtix7 (urj_chain_t *chain)
+{
+    printf("initArtix7\n");
+    printf("not tested\n");
+
+    urj_part_t *part = urj_tap_chain_active_part(chain);
+    assert(part != NULL && "part must not be NULL");
+    /// set instruction register length of part if database does not contain part?
+    urj_part_instruction_length_set (part, 6);
+
+
+
+    int user1register_register_length = 12; /// length of data register
+    char *user1register_register_name = "USER1REGISTER";
+
+    if(urj_part_data_register_define(part, user1register_register_name, user1register_register_length) != URJ_STATUS_OK)
+    {
+        printf("definition of register failed\n");
+        return -8;
+    }
+
+
+    urj_part_instruction_t *instr = urj_part_instruction_define(part, "USER1", "00110010", user1register_register_name);
+
+    if(!instr)
+    {
+        printf("defining instruction failed\n");
+        return -7;
+    }
+
+
+    /// load USER1 instruction
+    urj_part_set_instruction(part, "USER1");
+    urj_tap_chain_shift_instructions(chain);
+
+
+
+    /// do datashift data shift
+    urj_part_instruction_t *active_ir = part->active_instruction;
+    if (active_ir == NULL)
+    {
+        printf("5 ?????\n");
+        return -5;
+    }
+    urj_data_register_t *dreg = active_ir->data_register;
+    if (dreg == NULL)
+    {
+        printf("6 ?????\n");
+        return -6;
+    }
+    return 0;
+}
+
+int initEcp2 (urj_chain_t *chain)
+{
+    printf("initEcp2\n");
+    urj_part_t *part = urj_tap_chain_active_part(chain);
+    assert(part != NULL && "part must not be NULL");
+    /// set instruction register length of part if database does not contain part?
+    urj_part_instruction_length_set (part, 8);
+
+
+
+    int user1register_register_length = 12; /// length of data register
+    char *user1register_register_name = "USER1REGISTER";
+
+    if(urj_part_data_register_define(part, user1register_register_name, user1register_register_length) != URJ_STATUS_OK)
+    {
+        printf("definition of register failed\n");
+        return -8;
+    }
+
+
+    urj_part_instruction_t *instr = urj_part_instruction_define(part, "USER1", "00110010", user1register_register_name);
+
+    if(!instr)
+    {
+        printf("defining instruction failed\n");
+        return -7;
+    }
+
+
+    /// load USER1 instruction
+    urj_part_set_instruction(part, "USER1");
+    urj_tap_chain_shift_instructions(chain);
+
+
+
+    /// do datashift data shift
+    urj_part_instruction_t *active_ir = part->active_instruction;
+    if (active_ir == NULL)
+    {
+        printf("5 ?????\n");
+        return -5;
+    }
+    urj_data_register_t *dreg = active_ir->data_register;
+    if (dreg == NULL)
+    {
+        printf("6 ?????\n");
+        return -6;
+    }
+    return 0;
+}
+
+
 
 int initSpartan3(urj_chain_t *chain)
 {
@@ -147,6 +313,58 @@ int initSpartan3(urj_chain_t *chain)
     }
     return 0;
 }
+int initSpartan6(urj_chain_t *chain)
+{
+    printf("initSpartan6\n");
+    printf("not tested\n");
+    urj_part_t *part = urj_tap_chain_active_part(chain);
+    assert(part != NULL && "part must not be NULL");
+    /// set instruction register length of part if database does not contain part?
+    urj_part_instruction_length_set (part, 6);
+
+
+
+    int user1register_register_length = 12; /// length of data register
+    char *user1register_register_name = "USER1REGISTER";
+
+    if(urj_part_data_register_define(part, user1register_register_name, user1register_register_length) != URJ_STATUS_OK)
+    {
+        printf("definition of register failed\n");
+        return -8;
+    }
+
+
+    urj_part_instruction_t *instr = urj_part_instruction_define(part, "USER1", "000010", user1register_register_name);
+
+    if(!instr)
+    {
+        printf("defining instruction failed\n");
+        return -7;
+    }
+
+
+    /// load USER1 instruction
+    urj_part_set_instruction(part, "USER1");
+    urj_tap_chain_shift_instructions(chain);
+
+
+
+    /// do datashift data shift
+    urj_part_instruction_t *active_ir = part->active_instruction;
+    if (active_ir == NULL)
+    {
+        printf("5 ?????\n");
+        return -5;
+    }
+    urj_data_register_t *dreg = active_ir->data_register;
+    if (dreg == NULL)
+    {
+        printf("6 ?????\n");
+        return -6;
+    }
+    return 0;
+}
+
 int initiCE40(urj_chain_t *chain)
 {
     printf("initiCE40\n");
