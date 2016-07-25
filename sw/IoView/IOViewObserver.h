16fd7828ef26677c2fb9c655b89ea15acce973d3
EvaIPDBG <eva.kissling@comlab.ch>
2016-07-25 15:32:42 +0200

    IOView works on Windows

---
+++ sw/IoView/IOViewObserver.h
@@ -0,0 +1,14 @@
+#ifndef IOVIEWOBSERVER_H_INCLUDED
+#define IOVIEWOBSERVER_H_INCLUDED
+
+
+#include <cstdint>
+
+class IOViewPanelObserver
+{
+public:
+    virtual void setOutput(uint8_t *buffer, size_t len) = 0;
+};
+
+
+#endif // IOVIEWOBSERVER_H_INCLUDED
