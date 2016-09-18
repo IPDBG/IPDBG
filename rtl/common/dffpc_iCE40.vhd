58a8dc8d5e3c05f69ef98e2af06835ef2c830165
danselmi <danselmi@gmx.ch>
2016-09-18 18:30:44 +0200

    use ff with clock enable (as expected by interface) on iCE40 family

--- rtl/common/dffpc_iCE40.vhd
+++ rtl/common/dffpc_iCE40.vhd
@@ -18,10 +18,11 @@ end entity;
 
 architecture iCE40 of dffp is
 
-    component SB_DFF
+    component SB_DFFE
         port(
             d : in  std_logic;
             c : in  std_logic;
+            e : in  std_logic;
             q : out std_logic
         );
     end component;
@@ -30,9 +31,10 @@ begin
 
     SB_DFF_inst: SB_DFF
     port map (
+        E => ce,    -- clock enable
         Q => q,     -- Registered Output
-        C => clk,           -- Clock
-        D => d            -- Data
+        C => clk,   -- Clock
+        D => d      -- Data
     );
 
 
