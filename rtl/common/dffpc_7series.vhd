4dfd3765dbb750c46c18a831399d239debe3dd70
danselmi <danselmi@gmx.ch>
2016-09-18 18:36:29 +0200

    add primitives for xilinx 7 series

---
+++ rtl/common/dffpc_7series.vhd
@@ -0,0 +1,42 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+library  UNISIM;
+use  UNISIM.vcomponents.all;
+
+entity dffp is
+    port(
+        clk     : in  std_logic;
+        ce      : in  std_logic;
+
+        d       : in  std_logic;
+        q       : out std_logic
+    );
+end entity;
+
+
+architecture SevenSeries of dffp is
+
+
+begin
+
+    FF : FDRE
+        generic map
+        (
+            INIT => '0'
+        )
+        port map
+        (
+          Q  => q,
+          C  => clk,
+          CE => ce,
+          D  => d,
+          R  => '0'
+        );
+
+
+end architecture SevenSeries;
+
+
+
