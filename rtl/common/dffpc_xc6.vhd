fe4eb5ac2f88a7802e8c3ef6ff89dbc0cba87132
danselmi <danselmi@gmx.ch>
2017-10-24 18:30:40 +0200

    add dffpc for xc6 (copy of xc7)

---
+++ rtl/common/dffpc_xc6.vhd
@@ -0,0 +1,32 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+library  UNISIM;
+use  UNISIM.vcomponents.all;
+
+entity dffpc is
+    port(
+        clk : in  std_logic;
+        ce  : in  std_logic;
+        d   : in  std_logic;
+        q   : out std_logic
+    );
+end entity dffpc;
+
+architecture dffpc_Virtex6 of dffpc is
+begin
+    FF : FDRE
+        generic map
+        (
+            INIT => '0'
+        )
+        port map
+        (
+            Q  => q,
+            C  => clk,
+            CE => ce,
+            D  => d,
+            R  => '0'
+        );
+end architecture dffpc_Virtex6;
