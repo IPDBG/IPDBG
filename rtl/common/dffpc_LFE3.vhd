a759c345da4fd71022da9d8dc216aec7a117de20
Daniel Anselmi <danselmi@gmx.ch>
2019-01-04 00:28:35 +0100

    rename files and add more architectures for dffp

---
+++ rtl/common/dffpc_LFE3.vhd
@@ -0,0 +1,27 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+library ecp3;
+use ecp3.components.all;
+
+entity dffpc is
+    port(
+        clk : in  std_logic;
+        ce  : in  std_logic;
+        d   : in  std_logic;
+        q   : out std_logic
+    );
+end entity dffpc;
+
+architecture lfe3 of dffpc is
+begin
+    FF : FD1P3AX
+        port map
+        (
+            Q  => q,
+            CK => clk,
+            SP => ce,
+            D  => d
+        );
+end architecture lfe3;
