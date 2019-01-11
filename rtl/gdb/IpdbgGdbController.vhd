a57bb64982719dc3a374ee6ed6c0a773adea5453
Albrecht David <dal@COMLAB.local>
2019-01-11 12:18:29 +0100

    add lm32 gdb stub. (working over IpdbgGdbController yea)

--- rtl/gdb/IpdbgGdbController.vhd
+++ rtl/gdb/IpdbgGdbController.vhd
@@ -65,7 +65,8 @@ architecture behavioral of IpdbgGdbController is
     signal ack_wr                 : std_logic;
     signal ack_rd                 : std_logic;
 begin
-
+    process (clk) begin
+    if ce = '1' then
     gen_arst: if ASYNC_RESET generate begin
         arst <= rst;
         srst <= '0';
@@ -79,24 +80,34 @@ begin
         signal break_enable : std_logic;
         signal break_local  : std_logic;
     begin
-
         break <= break_local and break_enable;
+
         process (clk) begin
             if rising_edge(clk) then
-                break_local <= '0';
-                if fifo_full = '0' and data_dwn_valid = '1' then -- data received from jtag if
-                    break_local <= '1';
+                if ce = '1' then
+                    break_local <= '0';
+                    if fifo_full = '0' and data_dwn_valid = '1' then -- data received from jtag if
+                        break_local <= '1';
+                    end if;
                 end if;
             end if;
         end process;
 
-        process (clk) begin
-            if rising_edge(clk) then
-                if (break_local and break_enable) = '1' then
-                    break_enable <= '0'; -- clear after activation
-                end if;
-                if (cyc_i and stb_i) = '1' and we_i = '1' and adr_i = "1" and ack_wr = '0' then
-                    break_enable <= dat_i(0);
+        process (clk, arst) begin
+            if arst = '1' then
+                break_enable <= '1';
+            elsif rising_edge(clk) then
+                if srst = '1' then
+                    break_enable <= '1';
+                else
+                    if ce = '1' then
+                        if (break_local and break_enable) = '1' then
+                            break_enable <= '0'; -- clear after activation
+                        end if;
+                        if (cyc_i and stb_i) = '1' and we_i = '1' and adr_i = "1" and ack_wr = '0' then
+                            break_enable <= dat_i(0);
+                        end if;
+                    end if;
                 end if;
             end if;
         end process;
@@ -111,7 +122,6 @@ begin
         signal data_o_local                  : std_logic_vector(7 downto 0);
     begin
         dat_o <= x"00000" & "00" & TxReady & valid & data_o_local;
-
         process (clk, arst)
             procedure rd_reset_assignment is begin
                 valid <= '0';
@@ -127,28 +137,30 @@ begin
                 if srst = '1' then
                     rd_reset_assignment;
                 else
-                    fifo_read_data_enable_delayed <= fifo_read_data_enable;
-                    clear_valid <= '0';
-                    ack_rd <= '0';
+                    if ce = '1' then
+                        fifo_read_data_enable_delayed <= fifo_read_data_enable;
+                        clear_valid <= '0';
+                        ack_rd <= '0';
 
-                    if (cyc_i and stb_i) = '1' and we_i = '0' and ack_rd = '0' then
-                        ack_rd <= '1';
-                        if adr_i = "0" then
-                            clear_valid <= '1';
+                        if (cyc_i and stb_i) = '1' and we_i = '0' and ack_rd = '0' then
+                            ack_rd <= '1';
+                            if adr_i = "0" then
+                                clear_valid <= '1';
+                            end if;
                         end if;
-                    end if;
 
-                    if fifo_read_data_enable_delayed = '1' then
-                        data_o_local <= fifo_read_data;
-                        valid <= '1';
-                    elsif clear_valid = '1' then
-                        valid <= '0';
-                    end if;
+                        if fifo_read_data_enable_delayed = '1' then
+                            data_o_local <= fifo_read_data;
+                            valid <= '1';
+                        elsif clear_valid = '1' then
+                            valid <= '0';
+                        end if;
 
-                    if fifo_empty = '0' and valid = '0' and fifo_read_data_enable = '0' and fifo_read_data_enable_delayed = '0' then
-                        fifo_read_data_enable <= '1';
-                    else
-                        fifo_read_data_enable <= '0';
+                        if fifo_empty = '0' and valid = '0' and fifo_read_data_enable = '0' and fifo_read_data_enable_delayed = '0' then
+                            fifo_read_data_enable <= '1';
+                        else
+                            fifo_read_data_enable <= '0';
+                        end if;
                     end if;
                 end if;
             end if;
@@ -179,28 +191,30 @@ begin
                 if srst = '1' then
                     wr_reset_assignment;
                 else
-                    ack_wr_data_buffer <= '0';
-                    if (cyc_i and stb_i) = '1' and we_i = '1' and ack_wr = '0' and (empty = '1' or adr_i = "1") then
-                        ack_wr <= '1';
-                        if adr_i = "0" then
-                            data_i_local <= dat_i(data_i_local'range);
-                            ack_wr_data_buffer <= '1';
+                    if ce = '1' then
+                        ack_wr_data_buffer <= '0';
+                        if (cyc_i and stb_i) = '1' and we_i = '1' and ack_wr = '0' and (empty = '1' or adr_i = "1") then
+                            ack_wr <= '1';
+                            if adr_i = "0" then
+                                data_i_local <= dat_i(data_i_local'range);
+                                ack_wr_data_buffer <= '1';
+                            end if;
+                        else
+                            ack_wr <= '0';
                         end if;
-                    else
-                        ack_wr <= '0';
-                    end if;
 
-                    if ack_wr_data_buffer = '1' then
-                        empty <= '0';
-                    elsif data_up_valid_local = '1' then
-                        empty <= '1';
-                    end if;
+                        if ack_wr_data_buffer = '1' then
+                            empty <= '0';
+                        elsif data_up_valid_local = '1' then
+                            empty <= '1';
+                        end if;
 
-                    if data_up_ready = '1' and empty = '0' and data_up_valid_local = '0' then
-                        data_up_valid_local <= '1';
-                        data_up <= data_i_local;
-                    else
-                        data_up_valid_local <= '0';
+                        if data_up_ready = '1' and empty = '0' and data_up_valid_local = '0' then
+                            data_up_valid_local <= '1';
+                            data_up <= data_i_local;
+                        else
+                            data_up_valid_local <= '0';
+                        end if;
                     end if;
                 end if;
             end if;
@@ -213,7 +227,7 @@ begin
         generic map(
             EXTRA_LEVEL_COUNTER => false,
             ASYNC_RESET         => ASYNC_RESET,
-            MEM_DEPTH           => 8
+            MEM_DEPTH           => 4
         )
         port map(
             clk               => clk,
@@ -229,11 +243,13 @@ begin
 
     fill_fifo:  process(clk)begin
         if rising_edge(clk) then
-            fifo_write_data <= data_dwn;
-            if fifo_full = '0' then
-                fifo_write_data_enable <= data_dwn_valid;
-            else
-                fifo_write_data_enable <= '0';
+            if ce = '1' then
+                fifo_write_data <= data_dwn;
+                if fifo_full = '0' then
+                    fifo_write_data_enable <= data_dwn_valid;
+                else
+                    fifo_write_data_enable <= '0';
+                end if;
             end if;
         end if;
     end process;
