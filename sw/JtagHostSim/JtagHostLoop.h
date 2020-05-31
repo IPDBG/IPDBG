966aa361c7f26ef06ff598583dcbb334e9705d93
Daniel Anselmi <danselmi@gmx.ch>
2020-05-31 17:42:16 +0200

    define functions in header

--- sw/JtagHostSim/JtagHostLoop.h
+++ sw/JtagHostSim/JtagHostLoop.h
@@ -2,5 +2,7 @@
 #define JTAG_HOST_LOOP
 
 extern "C" int jtagHostLoop();
+extern "C" void set_data_to_jtag_host(uint32_t data);
+extern "C" uint32_t get_data_from_jtag_host(uint32_t unused);
 
 #endif
--- sw/JtagHostSim/JtagHostLoopVpi.cpp
+++ sw/JtagHostSim/JtagHostLoopVpi.cpp
@@ -1,11 +1,12 @@
 #include <vpi_user.h>
 #include <thread>
 
+#include "JtagHostLoop.h"
+
 extern "C"
 {
 //procedure set_data_to_jtag_host(data : integer);
-void set_data_to_jtag_host(uint32_t data);
-static int set_data_to_jtag_host_v(char *userdata)
+static int set_data_to_jtag_host_vpi(char *userdata)
 {
     vpiHandle systfref, args_iter, argh;
     struct t_vpi_value argval;
@@ -36,7 +37,7 @@ void register_set_data_to_jtag_host()
     tf_data.type        = vpiSysTask;
     tf_data.sysfunctype = 0;
     tf_data.tfname      = "$set_data_to_jtag_host";
-    tf_data.calltf      = set_data_to_jtag_host_v;
+    tf_data.calltf      = set_data_to_jtag_host_vpi;
     tf_data.compiletf   = 0;
     tf_data.sizetf      = 0;
     tf_data.user_data   = 0;
@@ -44,8 +45,7 @@ void register_set_data_to_jtag_host()
 }
 
 //function get_data_from_jtag_host(unused : integer) return integer;
-uint32_t get_data_from_jtag_host(uint32_t unused);
-static int get_data_from_jtag_host_v(char *userdata)
+static int get_data_from_jtag_host_vpi(char *userdata)
 {
     vpiHandle systfref, args_iter, argh;
     struct t_vpi_value argval;
@@ -66,27 +66,6 @@ static int get_data_from_jtag_host_v(char *userdata)
     // Cleanup and return
     vpi_free_object(args_iter);
     return 0;
-//    vpiHandle systfref, args_iter, argh;
-//    struct t_vpi_value argval;
-//
-//    // Obtain a handle to the argument list
-//    systfref = vpi_handle(vpiSysTfCall, NULL);
-//    args_iter = vpi_iterate(vpiArgument, systfref);
-//
-//    // Grab the value of the first argument
-//    argh = vpi_scan(args_iter);
-//    argval.format = vpiIntVal;
-//    vpi_get_value(argh, &argval);
-//    //vpi_printf("VPI routine received %d\n", value);
-//
-//    argval.value.integer = ;
-//    vpi_put_value(argh, &argval, NULL, vpiNoDelay);
-//
-//    // Cleanup and return
-//    vpi_free_object(args_iter);
-//    int32_t ret_val = static_cast<int32_t>(get_data_from_jtag_host(0) & 0x7fffffff);
-//    tf_putp(0, ret_val);
-//    return 0;
 }
 void register_get_data_from_jtag_host()
 {
@@ -95,23 +74,19 @@ void register_get_data_from_jtag_host()
     tf_data.type        = vpiSysTask;
     tf_data.sysfunctype = 0;
     tf_data.tfname      = "$get_data_from_jtag_host";
-    tf_data.calltf      = get_data_from_jtag_host_v;
+    tf_data.calltf      = get_data_from_jtag_host_vpi;
     tf_data.compiletf   = 0;
     tf_data.sizetf      = 0;
     tf_data.user_data   = 0;
     vpi_register_systf(&tf_data);
 }
 
-//static int hello_compiletf(char*user_data)
-//{
-//      return 0;
-//}
-
 int jtagHostLoop();
 
-static int JtagHostLoop_v(char *user_data)
+static int JtagHostLoop_vpi(char *user_data)
 {
-    std::thread *thread = new std::thread(jtagHostLoop);
+    //std::thread *thread =
+    new std::thread(jtagHostLoop);
     return 0;
 }
 
@@ -122,7 +97,7 @@ void register_JtagHostLoop()
     tf_data.type        = vpiSysTask;
     tf_data.sysfunctype = 0;
     tf_data.tfname      = "$jtaghostloop";
-    tf_data.calltf      = JtagHostLoop_v;
+    tf_data.calltf      = JtagHostLoop_vpi;
     tf_data.compiletf   = 0;
     tf_data.sizetf      = 0;
     tf_data.user_data   = 0;
