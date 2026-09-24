03de5d8f983baa30e7c523f8d81008496cb9be3a
Daniel Anselmi <danselmi@gmx.ch>
2026-09-25 00:38:33 +0200

    Remove JtagHostSim
    Superseded by CoSim. If we need a faster simulation in the feature,
    adjust CoSim to be able to switch between jtag/OpenOCD bitbang and
    a direct socket connection.

--- sw/JtagHostSim/JtagHostLoop.h
+++
@@ -1,8 +0,0 @@
-#ifndef JTAG_HOST_LOOP
-#define JTAG_HOST_LOOP
-
-extern "C" int jtagHostLoop();
-extern "C" void set_data_to_jtag_host(uint32_t data);
-extern "C" uint32_t get_data_from_jtag_host(uint32_t unused);
-
-#endif
